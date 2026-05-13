62a511705b29188dff767c9f254222390399321f
Brian Hague <bhague1281@gmail.com>
2026-05-13 06:53:44 -0700

    server side rendering code

--- week7/server-side-rendering/server.js
+++ week7/server-side-rendering/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import mustacheExpress from 'mustache-express';
 
 const server = express();
 
+server.engine('mustache', mustacheExpress());
+server.set('view engine', 'mustache');
+server.set('views', __dirname + '/views');
+
 server.use(express.json());
 server.get('/', (req, res) => {
-    res.send('Hello World!');
+    const items = [
+        {name: 'Ichiro statue' },
+        {name: 'Crab nachos in a ferry' },
+        {name: `Big Dumper's catcher helmet` },
+    ];
+
+    return res.render('index', { dateTime: new Date(), items, showItems: true });
 });
 
 export default server;
